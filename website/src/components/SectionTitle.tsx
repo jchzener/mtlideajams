453c3b8362c9003f3ea6e4b08df918f050d7727d
jchzener <hounkpe.jean@gmail.com>
2026-01-17 18:01:12 -0500

    refactor(website) branch

--- website/src/components/SectionTitle.tsx
+++
@@ -1,4 +0,0 @@
-// src/components/SectionTitle.tsx
-export default function SectionTitle({ children }: { children: string }) {
-  return <h2 className="font-serif text-2xl md:text-3xl mb-8">{children}</h2>;
-}
