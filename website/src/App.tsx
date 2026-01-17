8f778403e3fc6aa1905e45e0d46485cbbebddeec
jchzener <hounkpe.jean@gmail.com>
2026-01-17 09:47:54 -0500

    feat(ui): add ScrollToTop component to reset scroll position on navigation

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PastJamsPage from "./pages/PastJamsPage";
 import JamDetailPage from "./pages/JamDetailPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/jams" element={<PastJamsPage />} />
